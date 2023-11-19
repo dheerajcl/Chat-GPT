707d874d7f5ad19879e67449d1cc8a5654f64040
dheerajcl <cldheeraj541@gmail.com>
2023-11-19 12:46:55 +0530

    Deplyoment changes

--- server.js
+++ server.js
@@ -4,19 +4,18 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 require('dotenv').config();
 
-
 const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3040;
 
 const configuration = new Configuration({
   organization: "org-HAYLuBOVDIsjTvhmRYiBIMpw",
-  apiKey:"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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
 const corsOptions = {
-  origin: "*",
+  origin: "https://chat-gpt-xi-peach.vercel.app",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
@@ -28,23 +27,28 @@ app.get("/", (req, res) => {
   res.send("Welcome to the ChatGPT!");
 });
 
-
 app.post("/api", async (req, res) => {
+  console.log("Received POST request at /api");
   const { message } = req.body;
   console.log("Received message", message);
 
-  const response = await openai.createCompletion({
-    model: "gpt-3.5-turbo-instruct",
-    prompt: `${message}`,
-    max_tokens: 500,
-    temperature: 0.4,
-  });
-  const responseData = response.data.choices[0].text.trim();
-  console.log("Generated response", responseData);
+  try {
+    const response = await openai.createCompletion({
+      model: "gpt-3.5-turbo-instruct",
+      prompt: `${message}`,
+      max_tokens: 500,
+      temperature: 0.4,
+    });
+    const responseData = response.data.choices[0].text.trim();
+    console.log("Generated response", responseData);
 
-  res.json({
-    message: responseData,
-  });
+    res.json({
+      message: responseData,
+    });
+  } catch (error) {
+    console.error("Error generating response", error);
+    res.status(500).send("An error occurred while generating the response.");
+  }
 });
 
 app.listen(port, () => {
