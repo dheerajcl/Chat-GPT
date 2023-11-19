c23e11582d36477028d8b4805f37f6ada2f7c63d
dheerajcl <cldheeraj541@gmail.com>
2023-11-19 13:02:18 +0530

    Deplyoment changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const corsOptions = {
-  origin: "https://chat-gpt-xi-peach.vercel.app",
+  origin: "https://dheerajcl.github.io/Chat-GPT",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
